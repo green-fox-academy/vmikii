19e3e87d87b2c459ed828daf7ec3bca6c4514661
vmikii <veghmiki@gmail.com>
2018-10-29 16:24:44 +0100

    Square

---
+++ week-02/day-1/src/Square.java
@@ -0,0 +1,15 @@
+public class Square
+{
+    public static void main(String[] args)
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            if (i == 0 || i == 5)
+            {
+                System.out.println("%%%%%%");
+            }
+            else
+                System.out.println("%    %");
+        }
+    }
+}
