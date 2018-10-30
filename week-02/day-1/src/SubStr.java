7b0723d78a6359a64e1051104664087c6d7b8b90
vmikii <veghmiki@gmail.com>
2018-10-30 11:26:39 +0100

    Substr ref

--- week-02/day-1/src/SubStr.java
+++ week-02/day-1/src/SubStr.java
@@ -2,11 +2,11 @@ public class SubStr
 {
     public static void main(String[] args)
     {
-        System.out.println(SubString("This is what I'm searching in","searching"));
-        System.out.println(SubString("this is what I'm searching in","asd"));
+        System.out.println(SubStr("This is what I'm searching in","searching"));
+        System.out.println(SubStr("this is what I'm searching in","asd"));
     }
 
-    public static int SubString(String input, String q)
+    public static int SubStr(String input, String q)
     {
         if (!input.contains(q))
             return -1;
@@ -14,14 +14,12 @@ public class SubStr
         {
             char[] Input = input.toCharArray();
             char[] Q = q.toCharArray();
-            int j = 0;
             int i = 0;
-            while (Input[i] != Q[j] || Input[i + Q.length - 1] != Q[Q.length - 1])
+            while (Input[i] != Q[0] || Input[i + Q.length - 1] != Q[Q.length - 1])
             {
                 i++;
             }
             return i;
-
         }
     }
 }
