0f6c283ea81b0a61a064e1507a4c9ff47d839577
vmikii <veghmiki@gmail.com>
2018-11-08 11:34:09 +0100

    LinePlay

---
+++ week-03/day-5/src/LinePlay.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class LinePlay {
+    public static void mainDraw(Graphics graphics) {
+        int db = 16;
+        int space = WIDTH/db;
+
+        for (int i = 0; i <= WIDTH; i += space){
+            for (int j = 20; j <= WIDTH; j += space){
+                graphics.drawLine(0, i, i + space, 320 );
+                graphics.drawLine(i, 0, 320, i + space );
+            }
+        }
+
+
+    }
+
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
