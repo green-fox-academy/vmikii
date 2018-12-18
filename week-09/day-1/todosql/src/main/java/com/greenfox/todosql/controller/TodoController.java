7e43f3ee7a5580ee441f4b407123631c177c518d
vmikii <veghmiki@gmail.com>
2018-12-18 10:57:22 +0100

    Controller - list

--- week-09/day-1/todosql/src/main/java/com/greenfox/todosql/controller/TodoController.java
+++ week-09/day-1/todosql/src/main/java/com/greenfox/todosql/controller/TodoController.java
@@ -1,10 +1,13 @@
 package com.greenfox.todosql.controller;
 
+import com.greenfox.todosql.model.Todo;
 import com.greenfox.todosql.repository.TodoRepository;
 import com.greenfox.todosql.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -13,17 +16,16 @@ public class TodoController {
 
   TodoService service;
 
+  @Autowired
   public TodoController(TodoService service) {
     this.service = service;
   }
 
   @RequestMapping({"/", "/list"})
-  public String list(@RequestParam(value = "isActive",required = false) String isActive, Model model) {
-    if (isActive.equals("false")) {
-      model.addAttribute("todos", service.getRepository().findAll());
-    } else {
-      model.addAttribute("todos",service.getNotDoneTodos());
-    }
+  public String list(@RequestParam(value = "isActive",required = false) boolean isActive, Model model) {
+    model.addAttribute("todos", isActive
+        ? service.getAll()
+        : service.getActive());
     return "todolist";
   }
 }
